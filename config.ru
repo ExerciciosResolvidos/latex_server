d1bb161d895760ed51ea114205e377b27e458a2c
Luiz Zamboni <luizamboni2002@hotmaill.com>
2016-04-12 02:14:16 -0300

    RM logger

--- config.ru
+++ config.ru
@@ -4,9 +4,6 @@ require 'rack'
 #require 'byebug'
 require "latex_to_png"
 require "#{ROOT}/server"
-require "#{ROOT}/logger"
 
 Env = ENV['RACK_ENV'] || 'development'
-Logger.env = Env
-use Rack::CommonLogger,  Logger
 run Server.new
--- server.rb
+++ server.rb
@@ -4,14 +4,12 @@ require "latex_to_png"
 class Server
 
   ROOT_LIB = File.dirname __FILE__
-  
+
 
   def call(env)
-    Logger.write "path = #{env["PATH_INFO"]}\n"
     formula=  URI.unescape(env["PATH_INFO"].gsub(/.*formula\//, "").gsub(/\/size.*/,""))
     size =  env["PATH_INFO"].gsub(/.*size\//,"").to_i || 10
-    Logger.write "formula = #{formula}\n"
-    Logger.write "size = #{size}\n"
+
 # debugger
     begin
       tmp_file = LatexToPng::Convert.new(formula: formula,size: size ).to_png
@@ -31,7 +29,7 @@ class Server
       # gambiarra para caminho com /current/ no capistrano
       begin
         FileUtils.mkdir_p File.dirname( image_path)
-        FileUtils.cp(tmp_file.path, image_path) 
+        FileUtils.cp(tmp_file.path, image_path)
       rescue => e
         image_path = tmp_file.path
       end
